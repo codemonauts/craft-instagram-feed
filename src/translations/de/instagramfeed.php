e3f13660f9c93b729fd578557c5f7312363eb7ce
Karsten Schempp <karsten@schempp-online.de>
2019-03-20 16:55:28 +0100

    typo

--- src/translations/de/instagramfeed.php
+++ src/translations/de/instagramfeed.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
 	// Settings
 	'Username' => 'Benutzername',
-	'Please, enter your Instagram Username.' => 'Bitte geben Sie den Instagram Benutername ein.',
+	'Please, enter your Instagram Username.' => 'Bitte geben Sie den Instagram Benutzername ein.',
 	'Make sure the entered instagram user profil is not private.' => 'Stellen Sie sicher, dass der angegebene Benutzer auch ein öffentliches Profil besitzt.',
-
 ];
--- src/translations/en/instagramfeed.php
+++ src/translations/en/instagramfeed.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
 	// Settings
 	'Username' => 'Username',
-	'Please, enter your Instagram Username.' => 'Please, enter your Instagram Username.',
+	'Please, enter your Instagram Username.' => 'Please, enter your instagram username.',
 	'Make sure the entered instagram user profil is not private.' => 'Make sure your the entered instagram user profil is not private.',
-
 ];
