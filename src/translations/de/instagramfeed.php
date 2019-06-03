6841de35de5b111046aff86f779d48a42aad2027
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-06-03 12:24:06 -0700

    Typo fix: profil => profile

--- src/translations/de/instagramfeed.php
+++ src/translations/de/instagramfeed.php
@@ -4,5 +4,5 @@ return [
 	// Settings
 	'Username' => 'Benutzername',
 	'Please, enter your Instagram Username.' => 'Bitte geben Sie den Instagram Benutzername ein.',
-	'Make sure the entered instagram user profil is not private.' => 'Stellen Sie sicher, dass der angegebene Benutzer auch ein öffentliches Profil besitzt.',
+	'Make sure the entered Instagram user profile is not private.' => 'Stellen Sie sicher, dass der angegebene Benutzer auch ein öffentliches Profil besitzt.',
 ];
