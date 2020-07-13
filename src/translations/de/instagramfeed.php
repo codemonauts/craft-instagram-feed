8847200726565ee9427f58d1f0103dae7c5a1f03
Felix Breidenstein <felix@codemonauts.com>
2020-07-13 10:52:36 +0200

    Fix typo in translation

--- src/translations/de/instagramfeed.php
+++ src/translations/de/instagramfeed.php
@@ -3,6 +3,6 @@
 return [
 	// Settings
 	'Username' => 'Benutzername',
-	'Please, enter your Instagram Username.' => 'Bitte geben Sie den Instagram Benutzername ein.',
+	'Please, enter your Instagram username.' => 'Bitte geben Sie den Instagram Benutzername ein.',
 	'Make sure the entered Instagram user profile is not private.' => 'Stellen Sie sicher, dass der angegebene Benutzer auch ein öffentliches Profil besitzt.',
 ];
